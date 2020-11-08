ede72e7327dd7be027e977f0c3ec25c557fecf72
Boris Muñoz <bmunoz89@users.noreply.github.com>
2020-11-07 23:18:43 -0300

    feat: generate deck correctly

--- src/App.vue
+++ src/App.vue
@@ -62,10 +62,14 @@ export default defineComponent({
       })
     }
 
+    const cardItems: number[] = [1, 2, 3, 4, 5, 6, 7, 8]
+
     for (let i = 0; i < 16; i++) {
+      const cardItemIndex = i % 8
+
       cardList.value.push({
+        value: cardItems[cardItemIndex],
         position: i,
-        value: i % 8,
       })
     }
 
