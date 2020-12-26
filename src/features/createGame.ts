55cad2bb4e0e26eb4a8dfd18e7ed2e43900bceab
Boris Muñoz <bmunoz89@users.noreply.github.com>
2020-12-26 10:17:39 -0300

    chore: reduce bundle size of lodash functions

--- src/features/createGame.ts
+++ src/features/createGame.ts
@@ -1,4 +1,4 @@
-import { shuffle } from 'lodash'
+import shuffle from 'lodash/shuffle'
 import type { ComputedRef, Ref } from 'vue'
 import { computed, ref } from 'vue'
 import type { ICard } from './createDeck'
